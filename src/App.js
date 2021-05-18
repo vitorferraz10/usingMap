393a67ca0f31ddf668d3d51369e60b47117a2bac
Vitor Ferraz <vitor_ferrazmaciel@hotmail.com>
2021-05-18 15:00:59 -0300

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const luana = {
+  cliente: "Luana",
+  idade: 27,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: "Mario",
+  idade: 31,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+    { nome: "Guitarra", preco: "R$ 3500" },
+  ],
+  ativa: false,
+};
 
 function App() {
+  const dados = mario;
+ 
+
+  const total = dados.compras
+    .map((item) => +item.preco.replace("R$ ", ""))
+    .reduce((sum, item) => sum + item, 0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <section>
+        <p>Nome: {dados.cliente}</p>
+        <p>Idade: {dados.idade}</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Situação:{" "}
+          <span style={{ color: dados.ativa ? "green" : "red" }}>
+            {dados.ativa ? "Ativa" : "Inativa"}
+          </span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <p>Total gasto: {total}</p>
+        {total > 10000 && <p>'Voce está gastando muito'</p>}
+      </section>
+    </>
   );
 }
 
